53f764510d4a898d6cb7a8a2d6968956ca411667
Spitamen2018 <sukhrobazimov1996@gmail.com>
2023-11-20 11:15:30 +0500

    added readme

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Request;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
